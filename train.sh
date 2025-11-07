c6e0cf7b2460cd12973e2a807896d3e386f17a24
Pranshu Panda <58971586+pranshupanda@users.noreply.github.com>
2025-11-07 08:09:35 -0600

    Update email address for job notifications

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00          # A period of 24 hours is the maximum for one scheduling.
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=panda068@umn.edu  # Replace this with your own x500. Emails will be sent to notify the start and end of the program
+#SBATCH --mail-user=adeshwal@umn.edu  # Replace this with your own x500. Emails will be sent to notify the start and end of the program
 #SBATCH --output=terminal_output.txt  # This is a new file automatically created to record all the terminal outputs from this run.
 #SBATCH --mem=20G
 
