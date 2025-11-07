84dc8ce7f3410b1504df8e3be1cd708635638f49
pranshupanda <pranshu.panda@gmail.com>
2025-11-07 08:00:18 -0600

    added training script for MSI

---
+++ train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+#SBATCH --partition=a100-4
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --time=24:00:00          # A period of 24 hours is the maximum for one scheduling.
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=panda068@umn.edu  # Replace this with your own x500. Emails will be sent to notify the start and end of the program
+#SBATCH --output=terminal_output.txt  # This is a new file automatically created to record all the terminal outputs from this run.
+#SBATCH --mem=20G
+
+source ~/.bashrc
+conda activate gpuenv              # Replace these with your own environment
+
+PYTHONPATH="." python exp64.py
