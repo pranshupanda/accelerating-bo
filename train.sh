c6322fb23792913fea3807ca5495022ed73e588c
Pranshu Panda <58971586+pranshupanda@users.noreply.github.com>
2026-05-06 23:19:52 -0500

    Update email placeholder and change script name

--- train.sh
+++ train.sh
@@ -5,11 +5,11 @@
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00          # A period of 24 hours is the maximum for one scheduling.
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=panda068@umn.edu  # Replace this with your own x500. Emails will be sent to notify the start and end of the program
+#SBATCH --mail-user=<your_email>  # Replace this with your own x500. Emails will be sent to notify the start and end of the program
 #SBATCH --output=terminal_output.txt  # This is a new file automatically created to record all the terminal outputs from this run.
 #SBATCH --mem=20G
 
 source ~/.bashrc
 conda activate gpuenv              # Replace these with your own environment
 
-PYTHONPATH="." python exp64.py
+PYTHONPATH="." python batch64.py
